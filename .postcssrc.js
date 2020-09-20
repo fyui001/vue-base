a50af2d279f4dbb195e0de407923e99ea617809d
fyui001 <yun.cod@gmail.com>
2020-09-20 20:40:08 +0900

    postcssの設定

---
+++ .postcssrc.js
@@ -0,0 +1,11 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+    sourceMap: true,
+    plugins: {
+        'postcss-import': {},
+        'postcss-url': {},
+        autoprefixer: {},
+        cssnano: { preset: 'default', autoprefixer: false }
+    }
+}
